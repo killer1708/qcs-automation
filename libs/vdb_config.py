cb84bab58cc64552920c1ff569a68c1cafcdcfd6
Mithun Gore <mithun.gore@msystechnologies.com>
2018-05-14 11:50:19 +0530

    Change the names of conf files add cleanup of conf
    Conf file creation is modified and clean and backup
    of configuration file is added.
    
    Signed-off-by: Mithun Gore <mithun.gore@msystechnologies.com>

--- libs/vdb_config.py
+++ libs/vdb_config.py
@@ -2,6 +2,8 @@
 import re
 import random, string
 
+QCS_VDB_CONF = '/tmp/qcs_vdbench'
+
 def get_index(config_content, res_param):
     """
     Returns the index of line to append resource param.
@@ -23,17 +25,24 @@ def create_config(config_path, res_param, **kwargs):
     Open config file in write mode and add the contents
     :return:
     """
-    with open(config_path, 'r+') as f:
-        file_content = f.readlines()
+    with open(config_path, 'r') as input_conf, open(QCS_VDB_CONF, 'w') as out_conf:
+        file_content = input_conf.readlines()
+        if kwargs.get('vdbench_path'):
+            index = get_index(file_content, 'hd=default')
+            file_content.insert(index-1,
+                                'hd=default,vdbench={},user={},shell=ssh\n'.format(
+                                    kwargs.get('vdbench_path'),
+                                    kwargs.get('user', 'root'))
+                                )
+            file_content.remove(file_content[index])
         index = get_index(file_content, res_param)
         hd_name = ''.join(random.choice(string.lowercase) for x in range(6))
-        for host in kwargs.get('hostname', None):
+        for host in kwargs.get('hostname'):
             file_content.insert(index, 'hd={},system={}\n'.format(hd_name, host))
             index += 1
-        f.seek(0)
-        f.truncate()
         for line in file_content:
-            f.write(line)
+            out_conf.write(line)
+    return QCS_VDB_CONF
 
 if __name__ == '__main__':
-    create_config('qcsbench', res_param='hd=', hostname=['192.168.102.13'])
+    create_config('qcsbench', res_param='hd=', hostname=['192.168.102.13'], vdbench_path='/root/vdbench')
