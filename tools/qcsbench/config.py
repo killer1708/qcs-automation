5eb774a32191d358c30f628fcdb0c0286472cc0f
Mithun Gore <mithun.gore@msystechnologies.com>
2018-05-31 14:11:19 +0530

    Modify code related to multihost JVM
    -Added readme for master VM
    -Multi host support added
    -Added getting IP for master and stopping the firewall of master
    in Master configuration
    
    Signed-off-by: Mithun Gore <mithun.gore@msystechnologies.com>

---
+++ tools/qcsbench/config.py
@@ -0,0 +1,15 @@
+CLUSTER_NAME='newcl'
+TEMPLATE_NAME='automation-template'
+TEMPLATE_DS='data1'
+VM_NAME='qcs_vm'
+
+# slave vm details
+SLAVE_VM_COUNT=2
+SLAVE_UNAME='root'
+SLAVE_PASSWORD='master@123'
+
+# Master vm details
+MASTER_UNAME='root'
+MASTER_PASSWD='master#123'
+
+SAMPLE_VDB_CONFIG='/root/qcs_automation/libs/qcsbench'
