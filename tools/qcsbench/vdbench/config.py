ed51f5107217f23bf7d581e8c07fed9044ee34c8
Sandeep.Prajapati <sandeep.prajapati@msystechnologies.com>
2018-08-08 10:38:50 -0400

    1. Modifed vdbench and related code. 2. Also, updated logger.py to write on both - console and logfile
    Signed-off-by: Sandeep.Prajapati <sandeep.prajapati@msystechnologies.com>

--- tools/qcsbench/vdbench/config.py
+++ tools/qcsbench/vdbench/config.py
@@ -1,38 +1,63 @@
 # Ovirt Engine and host details
-#ovirt server ip
+# ovirt server ip
 OVIRT_ENGINE_IP = '192.168.103.65'
 
-#ovirt user name
+# ovirt user name
 OVIRT_ENGINE_UNAME = 'admin@internal'
 
-#ovirt password
+# ovirt password
 OVIRT_ENGINE_PASS = 'master@123'
 
-#ovirt cluster name
+# ovirt cluster name
 CLUSTER_NAME='QCSCL'
 
-#vm template name in ovirt
-TEMPLATE_NAME='automation_template2'
+# vm template name in ovirt
+# TEMPLATE_NAME='automation_template2'
+TEMPLATE_NAME='test_template_with_ip'
 
-#template data domain
+# template data domain
 TEMPLATE_DS='DATA_DOMAIN'
 
-#name of the vm to be created
+# name of the vm to be created
 VM_NAME='qcs_vm'
 
-#slave vm details
-#number vm to be create
-SLAVE_VM_COUNT=1
-#vm user name
+# slave vm details
+# number vm to be create
+SLAVE_VM_COUNT=2
+# slave vm type
+HOST_TYPE = 'linux'
+# vm user name
 SLAVE_UNAME='root'
-#vm password
+# vm password
 SLAVE_PASSWORD='master@123'
 
-# Master vm details
-#master vm user name
-MASTER_UNAME='root'
-#master vm password
-MASTER_PASSWD='master@123'
+# Current system details
+# system user name
+USERNAME = 'root'
+# system password 
+PASSWORD = 'master#123'
 
-#vdbench configuration file
+# vdbench configuration file
 SAMPLE_VDB_CONFIG='../../../libs/qcsbench'
+
+# specify load details
+# LOAD_TYPE possible values are - block_io and file_io
+LOAD_TYPE = 'block_io'
+# block io details
+WORKLOAD_INFO =  {
+                  "sd_params": {"threads":"1",
+                                "openflags":"o_direct"},
+                  "wd_params": {"rdpct":"0",
+                                "seekpct":"0",
+                                "xfersize":"4k"},
+                  "rd_params": {"elapsed":"10",
+                                "iorate" : "max"}
+                 }
+# file io details
+
+# log level
+LOG_LEVEL = "DEBUG"
+
+# disk details
+DISK_COUNT = 1
+DISK_SIZE_GB = 50
